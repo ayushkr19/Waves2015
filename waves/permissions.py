594792e12cb38909086495157b7d0d9e7f756be5
Ayush Kumar <ayushkr19@gmail.com>
2015-06-27 01:50:35 +0530

    Add IsOwnerOrSuperuser permission

--- waves/permissions.py
+++ waves/permissions.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from rest_framework import permissions
 from constants import *
 
@@ -37,14 +37,36 @@ class HasAtLeastOneGroupPermission(permissions.BasePermission):
         # Determine the required groups for this request method
         required_groups = required_groups_mapping.get(request.method, [])
 
-        print(request.user.is_anonymous())
-        print(request.method)
-        print(required_groups)
-        print(required_groups_mapping)
-
         # Return True if user is anonymous and anonymous users are allowed
         if request.user.is_anonymous() and ANONYMOUS_USER_GRP in required_groups:
             return True
 
         # Return True if user is in any of the group
         return any([is_in_group(request.user, group_name) for group_name in required_groups])
+
+class IsOwnerOrSuperuser(permissions.BasePermission):
+    """
+    Ensure whether user is the owner or the user is a superuser
+    """
+
+    def has_permission(self, request, view):
+
+        # Get username which has been passed from the URL
+        username = view.kwargs['username']
+
+        print('\n Username : ')
+        print(username)
+
+        # Retrieve user from the username
+        # Using the fact that filter returns QuerySets, and that usernames are unique
+        # Done so that this returns None instead of raising DoesNotExist exception
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            if request.user.is_superuser:
+                return True
+            else:
+                print(user == request.user)
+                return user == request.user
+        else:
+            return False
