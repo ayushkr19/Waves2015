7fd4e55ce79387351ecbfd22c3dae499b3431018
Ayush Kumar <ayushkr19@gmail.com>
2015-07-02 14:42:32 +0530

    Refactor admin site to show details of profiles

--- waves/admin.py
+++ waves/admin.py
@@ -4,4 +4,28 @@ from django.contrib import admin
 from waves.models import Event, Profile
 
 admin.site.register(Event)
-admin.site.register(Profile)
+
+def get_user_type(object):
+    """
+    Method to return user type
+    """
+    return object.get_user_type_display()
+# Setting these attributes for column name & sorting.
+get_user_type.short_description = 'User type'
+get_user_type.admin_order_field = 'user_type'
+
+def get_user_groups(object):
+    """
+    Return the groups that the user belongs to
+    """
+    return [str(group) for group in object.user.groups.all()]
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    To display profiles in a list. Easy on the eyes.
+    """
+    list_display = ('user', get_user_type, get_user_groups, 'phone_num', 'created_at', 'modified_at')
+    list_filter = ('user_type', 'created_at', 'modified_at')
+
+admin.site.register(Profile, ProfileAdmin)
